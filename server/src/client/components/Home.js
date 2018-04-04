0b0e47303ac7785004cad7e3bb1778f7ee9c8377
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-04-04 20:06:16 -0300

    creating a bundle for client

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>I'm the VERY home component</div>;
+  return (
+    <div>
+      <div>I'm the VERY home component</div>
+      <button onClick={() => console.log('Hi there!')}>Press me!</button>
+    </div>
+  );
 };
 
 export default Home;
---
+++ server/webpack.client.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/client/client.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { targets: { browsers: ['last 2 versions'] } }]
+          ]
+        }
+      }
+    ]
+  }
+};
