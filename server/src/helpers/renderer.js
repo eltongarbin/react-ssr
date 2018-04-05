b9879989a12040eebf33aacd7db873dc485f816e
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-04-05 20:39:45 -0300

    Adding Navigation

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom';
 
-import Home from './../client/components/Home';
+import Routes from '../client/Routes';
 
-export default () => {
-  const content = renderToString(<Home />);
-  
+export default ({ path }) => {
+  const content = renderToString(
+    <StaticRouter location={path} context={{}}>
+      <Routes />
+    </StaticRouter>
+  );
+
   return `
     <html>
       <head><head>
