984344f04ac6033f198158b83e8b6019466ef163
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-04-14 15:29:03 -0300

    Client Axios Instance

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -6,6 +6,11 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'api'
+});
 
 import Routes from './Routes';
 import reducers from './reducers';
@@ -13,7 +18,7 @@ import reducers from './reducers';
 const store = createStore(
   reducers,
   window.INITIAL_STATE,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk.withExtraArgument(axiosInstance))
 );
 
 ReactDOM.hydrate(
