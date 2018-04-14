23a131f62665ee82422d78d2e43a7faf1ece3d0b
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-04-14 15:41:24 -0300

    Server Axios Instance

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,7 @@ app.use(
   '/api',
   proxy('http://react-ssr-api.herokuapp.com', {
     proxyReqOptDecorator(opts) {
-      opts.header['x-forwarded-host'] = 'localhost:3000';
+      opts.headers['x-forwarded-host'] = 'localhost:3000';
       return opts;
     }
   })
@@ -21,7 +21,7 @@ app.use(
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  const store = createStore();
+  const store = createStore(req);
 
   const promises = matchRoutes(Routes, req.path).map(({ route }) => {
     return route.loadData ? route.loadData(store) : null;
