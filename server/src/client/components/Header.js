10cdb075b79aae0c61b8681ebc03824ac6d75743
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-04-14 16:46:58 -0300

    Header Customization & Styling

--- server/src/client/components/Header.js
+++ server/src/client/components/Header.js
@@ -5,10 +5,29 @@ import { connect } from 'react-redux';
 const Header = ({ auth }) => {
   console.log(auth);
 
+  const authButton = auth ? (
+    <a href="/api/logout">Logout</a>
+  ) : (
+    <a href="/api/auth/google">Login</a>
+  );
+
   return (
-    <div>
-      <Link to="/">React SSR</Link>
-    </div>
+    <nav>
+      <div className="nav-wrapper">
+        <Link to="/" className="brand-logo">
+          React SSR
+        </Link>
+        <ul className="right">
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+          <li>
+            <Link to="/admins">Admins</Link>
+          </li>
+          <li>{authButton}</li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
