7f949a5e515468e7bc999df4e3bea45253843aca
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-04-14 15:18:03 -0300

    api proxy setup

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,23 @@
 import 'babel-polyfill';
 import express from 'express';
 import { matchRoutes } from 'react-router-config';
+import proxy from 'express-http-proxy';
 
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 import Routes from './client/Routes';
 
 const app = express();
+
+app.use(
+  '/api',
+  proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+      opts.header['x-forwarded-host'] = 'localhost:3000';
+      return opts;
+    }
+  })
+);
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
