85dbfe4c8af591a129bf9b3cc4fc9e50c5bdb11e
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-04-14 16:17:20 -0300

    Calling FetchCurrentUser

--- server/src/client/App.js
+++ server/src/client/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { renderRoutes } from 'react-router-config';
 
 import Header from './components/Header';
+import { fetchCurrentUser } from './actions';
 
 const App = ({ route }) => {
   return (
@@ -13,5 +14,6 @@ const App = ({ route }) => {
 };
 
 export default {
-  component: App
+  component: App,
+  loadData: ({ dispatch }) => dispatch(fetchCurrentUser())
 };
