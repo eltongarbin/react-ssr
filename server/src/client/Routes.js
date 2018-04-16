19b584f1c8da1749ffca4bd6361b32482bde73ee
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-04-16 19:36:22 -0300

    Implementing the NotFound Page

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -3,6 +3,7 @@ import React from 'react';
 import App from './App';
 import HomePage from './pages/HomePage';
 import UsersListPage from './pages/UsersListPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 export default [
   {
@@ -16,6 +17,9 @@ export default [
       {
         ...UsersListPage,
         path: '/users'
+      },
+      {
+        ...NotFoundPage
       }
     ]
   }
