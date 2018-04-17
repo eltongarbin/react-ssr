2242c7c3a99e5b2b87f4e8e2f62fece5583aaaf5
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-04-16 21:04:50 -0300

    Require Auth in Use

--- server/src/client/pages/AdminsListPage.js
+++ server/src/client/pages/AdminsListPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchAdmins } from '../actions';
+import requireAuth from '../components/hocs/requireAuth';
 
 class AdminsListPage extends Component {
   componentDidMount() {
@@ -29,6 +30,8 @@ function mapStateToProps({ admins }) {
 }
 
 export default {
-  component: connect(mapStateToProps, { fetchAdmins })(AdminsListPage),
+  component: connect(mapStateToProps, { fetchAdmins })(
+    requireAuth(AdminsListPage)
+  ),
   loadData: ({ dispatch }) => dispatch(fetchAdmins())
 };
