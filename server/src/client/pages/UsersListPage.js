9767d15d9e7655f413e69910967eb511cd9cb8ee
Elton Diego Garbin do Nascimento <eltongarbin@gmail.com>
2018-04-18 21:35:59 -0300

    Setting Tags with Helmet

--- server/src/client/pages/UsersListPage.js
+++ server/src/client/pages/UsersListPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
 
 import { fetchUsers } from '../actions';
 
@@ -14,9 +15,19 @@ class UsersList extends Component {
     });
   }
 
+  head() {
+    return (
+      <Helmet>
+        <title>{`${this.props.users.length} Users Loaded`}</title>
+        <meta property="og:title" content="Users App" />
+      </Helmet>
+    );
+  }
+
   render() {
     return (
       <div>
+        {this.head()}
         Here's a big list of users: <ul>{this.renderUsers()}</ul>
       </div>
     );
